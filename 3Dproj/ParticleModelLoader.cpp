b3681698cd6c7e3967a3178154b5f8b773c40278
SimonStrands <36299106+SimonStrands@users.noreply.github.com>
2023-04-26 15:06:41 +0200

    fixed some small mistakes

--- 3Dproj/ParticleModelLoader.cpp
+++ 3Dproj/ParticleModelLoader.cpp
@@ -400,9 +400,9 @@ void loadParticleModel(std::vector<VolumetricVertex>& vertecies, const std::stri
 	//add more particle if needed
 	for(unsigned int f = 0; f < mesh->mNumFaces; f++){
 		//get one lenght that we will use for all
-		float l = vertexLenght(vertecies[mesh->mFaces[f].mIndices[0]], vertecies[mesh->mFaces[f].mIndices[1]]);
+		//float l = vertexLenght(vertecies[mesh->mFaces[f].mIndices[0]], vertecies[mesh->mFaces[f].mIndices[1]]);
 		//int R = l / nl;
-		int R = 3;
+		const int R = 1;
 	
 		VolumetricVertex tempArray[3] = {
 			vertecies[mesh->mFaces[f].mIndices[0]], 
