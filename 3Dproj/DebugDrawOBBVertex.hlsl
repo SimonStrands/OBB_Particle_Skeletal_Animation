245b75c633c2bb94fef8c59e39088dd32dd17169
SimonStrands <36299106+SimonStrands@users.noreply.github.com>
2023-04-27 11:43:10 +0200

    fixed bug where the OBB boxes doesn't display

--- 3Dproj/DebugDrawOBBVertex.hlsl
+++ 3Dproj/DebugDrawOBBVertex.hlsl
@@ -14,6 +14,12 @@ struct VertexShaderInput
 struct VertexShaderOutput
 {
     float4 position : SV_POSITION;
+    float2 uv : UV;
+    float4 color : COLOR;
+    float3 normal : NORMAL;
+    float3 tangent : TANGENT;
+    float3 bitangent : BITANGENT;
+    float4 fragpos : FRAG_POS;
 };
 
 VertexShaderOutput main(VertexShaderInput input, uint instanceID : SV_InstanceID)
@@ -21,5 +27,9 @@ VertexShaderOutput main(VertexShaderInput input, uint instanceID : SV_InstanceID
     VertexShaderOutput output;
     float4x4 MVP = mul(mul(transform[instanceID], view), projection);
     output.position = mul((float4(input.position, 1.0f)), MVP);
+    output.uv = float2(0.5f, 0.5f);
+    output.color = float4(1, 1, 1, 1);
+    output.normal = output.tangent = output.bitangent = float3(1, 1, 1);
+    output.fragpos = float4(input.position, 1.f);
     return output;
 }
